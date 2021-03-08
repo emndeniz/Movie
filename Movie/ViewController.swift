3fcb165ab441bf8b15df7648d06557a997300981
Emin DENİZ <emin@zeka.io>
2021-03-08 21:03:38 +0300

    Initial Commit

---
+++ Movie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie
+//
+//  Created by Mehmet Emin Deniz on 8.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
